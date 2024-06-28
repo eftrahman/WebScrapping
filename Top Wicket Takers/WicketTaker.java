5a6f5f0dc557e97e60981843f27ad50e8f0978e6
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-06-28 23:31:22 +0600

    Update WicketTaker.java

--- Top Wicket Takers/WicketTaker.java
+++ Top Wicket Takers/WicketTaker.java
@@ -1,1 +1,23 @@
+public class WicketTaker {
+    private String name;
+    private String wickets;
+    private String country;
 
+    public WicketTaker(String name, String wickets, String country) {
+        this.name = name;
+        this.wickets = wickets;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWickets() {
+        return wickets;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
