dd38c4059cc39a3b7e99ca7785309d804e1d2091
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-06-30 22:26:28 +0600

    Update FootballScorer.java

--- FootballScorer01/FootballScorer.java
+++ FootballScorer01/FootballScorer.java
@@ -1,1 +1,24 @@
+public class FootballScorer {
+    private String name;
+    private String goals;
+    private String club;
+
+    public FootballScorer(String name, String goals, String club) {
+        this.name = name;
+        this.goals = goals;
+        this.club = club;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+
+    public String getClub() {
+        return club;
+    }
+}
 
