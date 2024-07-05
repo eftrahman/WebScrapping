f3082bcbb7a0e12b504bd80460d7c06dd1eb3638
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-07-05 23:56:13 +0600

    Update TVSeries.java

--- TopRatedTVSeries/TVSeries.java
+++ TopRatedTVSeries/TVSeries.java
@@ -1,1 +1,24 @@
+public class Movie {
+    private String title;
+    private String rating;
+    private String year;
+
+    public Movie(String title, String rating, String year) {
+        this.title = title;
+        this.rating = rating;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
 
