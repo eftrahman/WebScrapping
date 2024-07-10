d961ed129112710384bdd31e167306fc5cd1cd40
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-07-10 23:58:42 +0600

    Update TennisPlayer.java

--- Top Women's Tennis Player Ranking /TennisPlayer.java
+++ Top Women's Tennis Player Ranking /TennisPlayer.java
@@ -1,1 +1,23 @@
+public class TennisPlayer {
+    private String name;
+    private String ranking;
+    private String points;
 
+    public TennisPlayer(String name, String ranking, String points) {
+        this.name = name;
+        this.ranking = ranking;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRanking() {
+        return ranking;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+}
